94e63c76c8999082403053a9502a02f46f85968e
yvesbou <63062257+yvesbou@users.noreply.github.com>
2022-05-05 11:44:33 +0200

    added chakra UI to the project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import { ChakraProvider, CSSReset } from '@chakra-ui/react'
+import theme from '../styles/theme/chakra_theme'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <CSSReset/>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
+
 
 export default MyApp
